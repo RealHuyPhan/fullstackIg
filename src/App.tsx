9b67e024a50b512b2c5293774e9dde49d344d047
Huy Phan <huyps.bhsoft@gmail.com>
2023-05-16 17:39:28 +0700

    Create avatar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
+import { CircleAvatar } from './components/circle_avatar';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <CircleAvatar
+        url={'https://ict-imgs.vgcloud.vn/2020/09/01/19/huong-dan-tao-facebook-avatar.jpg'}
+        size={56}
+        onClick={function(): void{
+          throw new Error("Function not implemented!");
+        }}
+      />
     </div>
   );
 }
